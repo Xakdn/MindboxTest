8d2ac72d08837d1333f4cf26fdd08b14358b9176
Valeriy Volkov <valeriysvolkov@gmail.com>
2022-04-06 01:14:32 +0300

    Added distinct to SQL query

--- MindboxTest.sql
+++ MindboxTest.sql
@@ -1,4 +1,4 @@
-SELECT p.ProductName, c.CategoryName
+SELECT DISTINCT p.ProductName, c.CategoryName
 FROM ProductCategories pc 
 INNER JOIN Categories c ON pc.CategoryId = c.CategoryId 
 RIGHT JOIN Products p ON pc.ProductId = p.ProductId
